11385911ff2a3c4e4df888c9e42793aad5809961
Mariano Molina <mariano@contenidos-digitales.com>
2015-10-15 12:43:18 -0300

    Se puede calcular una suma en pesos ARS y pasar a BTC

--- test/ViewDetailArsController.swift
+++ test/ViewDetailArsController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewDetailArsController: UIViewController {
     
+    @IBOutlet weak var inputPesosValue: UITextField!
+    @IBOutlet weak var btnCalc: UIButton!
+    @IBOutlet weak var resultView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        // Add action
+        btnCalc.addTarget(self, action: "btnClicked:", forControlEvents: .TouchUpInside)
         
-        
-        // lallalala
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func btnClicked(sender: UIButton) {
+        // tomamos el valor
+        let sumaCalc: String = inputPesosValue.text!
+        
+//        if let _: String = sumaCalc {
+//            let cantidadDePesos = Double.init(sumaCalc)
+//            let resultado = calcularValorBTC(cantidadDePesos!, cotizacionUSD: 15.84, cotizacionBTC: 253.32)
+//            resultView.text = "\(resultado) BTC"
+//        } else {
+//            print("No tiene nada... fijate que hacer")
+//        }
+        
+        guard let _: String = sumaCalc else {
+            print("No tiene nada... fijate que hacer")
+        }
+        
+        let cantidadDePesos = Double.init(sumaCalc)
+        let resultado = calcularValorBTC(cantidadDePesos!, cotizacionUSD: 15.84, cotizacionBTC: 253.32)
+        resultView.text = "\(resultado) BTC"
+        
     }
     
     func calcularValorBTC(cantidadARS: Double, cotizacionUSD: Double, cotizacionBTC: Double) -> Double {
@@ -33,7 +60,7 @@ class ViewDetailArsController: UIViewController {
         let pesosToUSD = cantidadARS / cotizacionUSD
         // pasamos los USD a BTC
         result = pesosToUSD / cotizacionBTC
-        // retornamos el resultado de la operacion
+        // retornamos el resultado de la operacion3
         return result
     }
 }
