9b2a54b6bec3268fbbed889562dc0c446c6adf94
Mariano Molina <mariano@contenidos-digitales.com>
2015-10-23 17:28:45 -0300

    :bug: en la vista que calcula los BTC a USD

--- test/ViewDetailBtcController.swift
+++ test/ViewDetailBtcController.swift
@@ -2,26 +2,34 @@
 //  ViewDetailBtcController.swift
 //  test
 //
-//  Created by Mariano Molina on 10/15/15.
+//  Created by Mariano Molina on 10/23/15.
 //  Copyright © 2015 Mariano Molina. All rights reserved.
 //
 
 import UIKit
 
 class ViewDetailBtcController: UIViewController {
-    
-    @IBOutlet weak var resultView: UILabel!
-    @IBOutlet weak var inputBTC: UITextField!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func calcularBtcValue(sender: UIButton) {
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
+    */
+
 }
