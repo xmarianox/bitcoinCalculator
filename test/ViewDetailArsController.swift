1aa36c791997e97c8c8824cf114e3580c02c1c76
Mariano Molina <xsin.crueldadx@gmail.com>
2015-10-22 23:52:25 -0300

    Se agrego la logica para traer la cotizacion y en base a ese dato se calcula el importe en BTC :rocket:

--- test/ViewDetailArsController.swift
+++ test/ViewDetailArsController.swift
@@ -13,11 +13,25 @@ class ViewDetailArsController: UIViewController {
     @IBOutlet weak var inputPesosValue: UITextField!
     @IBOutlet weak var btnCalc: UIButton!
     @IBOutlet weak var resultView: UILabel!
+    var contizacionActual: Double!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Add action
-        btnCalc.addTarget(self, action: "btnClicked:", forControlEvents: .TouchUpInside)
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)) {
+            let cotizador = CotizadorService()
+            let miCotizacion = cotizador.getCotizacionAtAlias("ARS")
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                self.contizacionActual = miCotizacion
+            }
+        }
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        // mostramos el teclado
+        inputPesosValue.becomeFirstResponder()
     }
     
     override func didReceiveMemoryWarning() {
@@ -25,6 +39,32 @@ class ViewDetailArsController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func clickCalcularBTC(sender: UIButton) {
+        
+        let sumaACalc = inputPesosValue.text!
+        
+        if let valorNumerico = convertirADouble(sumaACalc, utilizaPuntoDeMiles: false) {
+            let calculo = valorNumerico * contizacionActual
+        
+            resultView.text = "\(calculo)"
+        } else {
+            resultView.text = "Es necesario ingresar un numero"
+        }
+        
+    }
+    
+    func convertirADouble(valor: String, utilizaPuntoDeMiles: Bool) -> Double? {
+        
+        let formatter = NSNumberFormatter()
+        formatter.usesGroupingSeparator = utilizaPuntoDeMiles
+        formatter.locale = NSLocale.currentLocale()
+        
+        let decimalAsDouble = formatter.numberFromString(valor)?.doubleValue
+        
+        return decimalAsDouble
+    }
+    
+    /*
     func btnClicked(sender: UIButton) {
         // tomamos el valor
         let sumaCalc: String = inputPesosValue.text!
@@ -53,4 +93,6 @@ class ViewDetailArsController: UIViewController {
         // retornamos el resultado de la operacion3
         return result
     }
+    */
+    
 }
